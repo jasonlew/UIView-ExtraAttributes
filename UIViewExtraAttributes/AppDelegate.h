b091dedad667614f97c1e54db13b8bd06db2eac7
Jason Lew <jlew.project0@gmail.com>
2012-08-28 05:17:52 -0700

    Initial Commit

---
+++ UIViewExtraAttributes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIViewExtraAttributes
+//
+//  Created by jlew on 8/28/12.
+//  Copyright (c) 2012 jlew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIViewExtraAttributes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIViewExtraAttributes
+//
+//  Created by jlew on 8/28/12.
+//  Copyright (c) 2012 jlew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
